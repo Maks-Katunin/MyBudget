5d80f3332a93e48326098c83e31609b790248bb3
Maks-Katunin <88392581+Maks-Katunin@users.noreply.github.com>
2026-07-18 22:11:13 +0100

    Update firebase-config.js
    Connect MyBudget to new Firebase project

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASN1PL0PhQHj0odFDgGO5dm_eBvZOWlAs",
-  authDomain: "mybudget-48ab5.firebaseapp.com",
-  projectId: "mybudget-48ab5",
-  storageBucket: "mybudget-48ab5.firebasestorage.app",
-  messagingSenderId: "457608940301",
-  appId: "1:457608940301:web:2c8e7d9220fad0ec76be44",
+  apiKey: "AIzaSyDTvRUtQ9lxIa62bNidT0ylcYAtVulyxNo",
+  authDomain: "mybudget-785b8.firebaseapp.com",
+  projectId: "mybudget-785b8",
+  storageBucket: "mybudget-785b8.firebasestorage.app",
+  messagingSenderId: "999065641446",
+  appId: "1:999065641446:web:004381bacf20e529e13627",
 };
 
 const app = initializeApp(firebaseConfig);
